095120a271dde59b176f9c29691d56b6b35c9842
Murilo Santos <muriloue@gmail.com>
2022-07-15 00:40:32 -0300

    Env variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,25 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLfv1X4V4dRYQVKQYKl_O_hgx7-cG8aCM",
-  authDomain: "cenolab-responsive.firebaseapp.com",
-  projectId: "cenolab-responsive",
-  storageBucket: "cenolab-responsive.appspot.com",
-  messagingSenderId: "991699974195",
-  appId: "1:991699974195:web:27e3d0a0791fd5c7980b46",
-  measurementId: "G-LMEHSZEHZP"
+  // @ts-ignore
+  apiKey: import.meta.env.VITE_API_KEY,
+  // @ts-ignore
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  // @ts-ignore
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  // @ts-ignore
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  // @ts-ignore
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  // @ts-ignore
+  appId: import.meta.env.VITE_APP_ID,
+  // @ts-ignore
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
-
-// SUBSTITUIR PARA PRODUCTION:
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_ID,
-//   measurementId: import.meta.env.VITE_MEASUREMENT_ID
-// };
 
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
